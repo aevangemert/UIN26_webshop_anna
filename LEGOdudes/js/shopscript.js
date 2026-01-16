d5f2f1b83f17b73815fe78b3a590e9e149203fb6
aevangemert <anna.e.gemert@hiof.no>
2026-01-16 13:51:35 +0100

    forelesning

--- LEGOdudes/js/shopscript.js
+++ LEGOdudes/js/shopscript.js
@@ -3,7 +3,6 @@ document.getElementById("cart-button").addEventListener("click", function () {
 })
 
 //Funksjon for produktopplisting:
-
 function fetchProducts() {
     let productHTML = ""
     products.map(p => productHTML += `<article class="product-card">
@@ -19,10 +18,69 @@ function fetchProducts() {
 
 fetchProducts()
 
+//Generer handlevogn
+function showCart() {
+    //Unike produkter
+    let uniqueItems = new Set(cart)
+    let uniqueArray = [...uniqueItems]
+    //Oversikt over antall per produkt
+    let cartItems = []
+    uniqueArray.map(item => {
+
+        cartItems.push({ prodid: item, quantity: cart.filter(i => i === item).length })
+    })
+
+    //Gå gjennom cartItems for å lage HTML til handlekurven og regne ut totalpris
+    let cartHTML = ""
+    let totalPrice = 0
+
+    //ci = cartItems
+    cartItems.map(ci => {
+        //Hente produktinformasjonen
+        //Bruker let konsekvent fordi det skal kjøre flere ganger, bytter etterhvert?
+        let product = products.find(i => i.prodid === ci.prodid)
+        //Skrive ut HTML
+        cartHTML += `<tr>
+                    <td class="title">${product.title}</td>
+                    <td class="price">${product.price}</td>
+                    <td class="quantity">${ci.quantity}</td>
+                    <td class="delete"><button onClick="deleteFromCart(${product.prodid})">X</button></td>
+                </tr>`
+        //Summere totalpris
+        //Legge til det som allerede finnes der
+        totalPrice += Number(product.price) * Number(ci.quantity)
+    })
+
+    if(cart.length === 0){
+        cartHTML += "<tr><td>Ingen varer i handlevognen.</td></tr>"
+    }
+
+    //Oppdatere HTML-elementer
+    document.getElementById("cart-items").innerHTML = cartHTML
+    document.getElementById("total-price").innerHTML = totalPrice
+    document.getElementById("cart_quantity").innerHTML = cart.length
+
+}
+
+//Slette fra handlevogn
+function deleteFromCart(prodid){
+    //Variabel
+    let deleteIndex = cart.indexOf(prodid)
+    if(deleteIndex > -1){
+        cart.splice(deleteIndex, 1)
+    }
+    //Oppdatere handlevogn-utskrift
+    showCart()
+}
+
+
 //Legg til i handlevogn
-function addToCart(prodid){
+function addToCart(prodid) {
     console.log("Legg til produkt med id: " + prodid)
     cart.push(prodid)
     console.log(cart)
-    document.getElementById("cart_quantity").innerHTML = cart.length
+
+    //Oppdater handlevisning (må generere HTML hver gang vi kjører showCart)
+    showCart()
 }
+
